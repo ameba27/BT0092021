5255892484d0f20b886bbbfb3b5449e4776c5f29
ameba27 <amadou.ba2@uvs.edu.sn>
2022-01-27 18:50:51 +0000

    art

---
+++ arts.php
@@ -0,0 +1,135 @@
+<?php
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css"
+      rel="stylesheet"
+    />
+    <!-- Latest compiled JavaScript -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+
+    <link rel="stylesheet" href="index.css">
+    <link rel="stylesheet" href="w3.css">
+    <link rel="stylesheet" href="./font/css/all.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+</head>
+<body>
+    <div class="x3-display-container w3-purple fixed-top">
+           
+           <div class="header" style="height: 40px">
+               <ul class="menu">
+                   <li><a href="index.php" class="w3-bar-item w3-button w3-purple w3-small"><i class="fas fa-home"></i>Home</a></li>
+                   <li><a href="" class="w3-bar-item w3-button w3-small w3-hover-text-white" >Répertoire</a></li>
+                   <li><a href="" class="w3-bar-item w3-button w3-small w3-hover-text-white">Informations</a></li> 
+                   <li><a href="afficheTab.php" class="w3-bar-item w3-button w3-small w3-hover-text-white">Gestion des profils</a></li>
+                   <li><a href="contact.php" class="w3-bar-item w3-button w3-small w3-hover-text-white">Contact</a></li>
+               </ul>
+                <ul class="inscription">
+                   <li><a href="formulaire.php" class="w3-animate-right w3-bar-item w3-button w3-small w3-hover-text-white ">Inscription</a></li>
+                   <li><a href="login.php" class="w3-animate-right w3-bar-item w3-button w3-small w3-hover-text-white">Connexion</a></li>
+                </ul>
+           </div>
+    </div>
+    <div class="w3-display-container">
+        <img src="./art/nature.jpg" alt="" style="width: 100%; height: 2000px">
+        <div class="w3-display-container  w3-display-topmiddle w3-center" style="margin-top: 70px; width: 100%, height: 700px;">
+            <h1 class="w3-text-purple " style="font-weight: 900; height: 60px; font-size: 3.5rem"><strong class="w3-text-orange">P'</strong>ART</h1>
+            
+            
+        </div><br>
+        <div class="w3-display-container  w3-display-topmiddle  w3-center W3-card" style="margin-top: 150px; margin-bottom: 100px; width: 100%, height: 700px; display: flex; justify-content: space-around ">
+        
+            <img  src="./art/bela.jpg" alt="" style="width: 45%; height: 320px; margin: 20px" >
+                
+                
+            <img  src="./art/blak.jpg" alt="" style="width: 45%; height: 320px; margin: 20px" >
+                
+                
+            <img  src="./art/lion.jpg" alt="" style="width: 45%; height: 320px; margin: 20px" >
+        
+                
+            <img  src="./art/peint.jpeg" alt="" style="width: 45%; height: 320px; margin: 20px" >
+            
+            
+        </div>
+        <div class="w3-display-container  w3-display-topmiddle  w3-center W3-card " style="margin-top: 500px; margin-bottom: 100px; width: 100%, height: 700px; display: flex; justify-content: space-around ">
+        
+        <img  src="./art/femafrik.jpg" alt="" style="width: 45%; height: 320px; margin: 20px" >
+            
+            
+        <img  src="./art/masque.jpg" alt="" style="width: 45%; height: 320px; margin: 20px" >
+            
+            
+        <img  src="./art/trafricain.jpg" alt="" style="width: 45%; height: 320px; margin: 20px" >
+    
+            
+        <img  src="./art/goodColor.jpg" alt="" style="width: 45%; height: 320px; margin: 20px" >
+        
+        
+        </div>
+        
+        <div class="w3-display-container  w3-display-topmiddle  w3-center W3-card " style="margin-top: 850px; margin-bottom: 100px; width: 100%, height: 700px; display: flex; justify-content: space-around ">
+        
+        <img  src="./art/oeil.jpg" alt="" style="width: 45%; height: 320px; margin: 20px" >
+            
+            
+        <img  src="./art/position.jpg" alt="" style="width: 45%; height: 320px; margin: 20px" >
+            
+            
+        <img  src="./art/perd.jpg" alt="" style="width: 45%; height: 320px; margin: 20px" >
+    
+            
+        <img  src="./art/vibs.jpg" alt="" style="width: 45%; height: 320px; margin: 20px" >
+        
+        
+        </div>
+        <div class="w3-display-container  w3-display-topmiddle  w3-center W3-card " style="margin-top: 1200px; margin-bottom: 100px; width: 100%, height: 700px; display: flex; justify-content: space-around ">
+        
+        <img  src="./art/Monalisa.jpg" alt="" style="width: 45%; height: 320px; margin: 20px" >
+            
+            
+        <img  src="./art/soufr.jpg" alt="" style="width: 45%; height: 320px; margin: 20px" >
+            
+            
+        <img  src="./art/inconu.jpeg" alt="" style="width: 45%; height: 320px; margin: 20px" >
+    
+            
+        <img  src="./art/cerf.jpg" alt="" style="width: 45%; height: 320px; margin: 20px" >
+        
+        
+        </div>
+        <div class="w3-display-container  w3-display-topmiddle  w3-center W3-card " style="margin-top: 1550px; margin-bottom: 100px; width: 100%, height: 700px; display: flex; justify-content: space-around ">
+        
+        <img  src="./art/fleur.jpg" alt="" style="width:45%; height: 400px; margin: 20px" >
+            
+            
+        <img  src="./art/dernier.jpg" alt="" style="width:45%; height: 400px; margin: 20px" >
+            
+            
+        <img  src="./art/abstract.jpg" alt="" style="width:45%; height: 400px; margin: 20px" >
+    
+            
+        <img  src="./art/artipnos.png" alt="" style="width: 45%; height: 400px; margin: 20px" >
+        
+        
+        </div>
+        
+                
+            
+        
+        
+    </div>
+
+
+
+
+    <div class="w3-container-bottom w3-purple w3-center w3-display-bottom  " style="width: 100%; margin: 0 auto; padding: 10px">
+        
+        &copy Souleymane & Amadou BootCamp 2021
+    </div>
+</body>
+</html>
